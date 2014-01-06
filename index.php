4454b0bfda6fea7cf1e89524b625196485a5c408
Juan Facundo Suarez <facundo@svarez.com.ar>
2014-01-06 13:44:53 -0300

    adding logo

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 </head>
 <body>
   <div class="top_image">
+    <div class="top_logo"></div>
+    <div class="title_under_logo">TAKE A STAND FOR WINTER<br />GET HOOKED UP LIKE A PRO</div>
   </div>
 </body>
 </html>
