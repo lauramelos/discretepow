91b145121b20c66db091a18c8ec0692073ef8dbb
Juan Facundo Suarez <facundo@svarez.com.ar>
2014-01-06 22:24:58 -0300

    adding fb app

--- index.php
+++ index.php
@@ -6,6 +6,26 @@ $facebook = new Facebook(array(
   'appId'  => '191151347752624',
   'secret' => 'a4c1147272a2df8f706f5ac2a7956b5b',
 ));
+
+
+$user = $facebook->getUser();
+if ($user) {
+  try {
+    // Proceed knowing you have a logged in user who's authenticated.
+    $user_profile = $facebook->api('/me');
+  } catch (FacebookApiException $e) {
+    error_log($e);
+    $user = null;
+  }
+}
+// Login or logout url will be needed depending on current user state.
+if ($user) {
+  $logoutUrl = $facebook->getLogoutUrl();
+} else {
+  $statusUrl = $facebook->getLoginStatusUrl();
+  $loginUrl = $facebook->getLoginUrl();
+}
+
 ?>
 
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
@@ -20,5 +40,32 @@ $facebook = new Facebook(array(
       <p>GET HOOKED UP LIKE A PRO</p>
     </div>
   </div>
+
+    <?php if ($user): ?>
+      <a href="<?php echo $logoutUrl; ?>">Logout</a>
+    <?php else: ?>
+      <div>
+        Check the login status using OAuth 2.0 handled by the PHP SDK:
+        <a href="<?php echo $statusUrl; ?>">Check the login status</a>
+      </div>
+      <div>
+        Login using OAuth 2.0 handled by the PHP SDK:
+        <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
+      </div>
+    <?php endif ?>
+
+    <h3>PHP Session</h3>
+    <pre><?php print_r($_SESSION); ?></pre>
+
+    <?php if ($user): ?>
+      <h3>You</h3>
+      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+
+      <h3>Your User Object (/me)</h3>
+      <pre><?php print_r($user_profile); ?></pre>
+    <?php else: ?>
+      <strong><em>You are not Connected.</em></strong>
+    <?php endif ?>
+
 </body>
 </html>
