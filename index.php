15fd8fd5b4779b9a2970083b59e2bfb9cdb85fb2
Juan Facundo Suarez <facundo@svarez.com.ar>
2014-01-06 22:28:23 -0300

    adding fb app

--- index.php
+++ index.php
@@ -58,11 +58,7 @@ if ($user) {
     <pre><?php print_r($_SESSION); ?></pre>
 
     <?php if ($user): ?>
-      <h3>You</h3>
       <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
-
-      <h3>Your User Object (/me)</h3>
-      <pre><?php print_r($user_profile); ?></pre>
     <?php else: ?>
       <strong><em>You are not Connected.</em></strong>
     <?php endif ?>
