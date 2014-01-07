332c77909b54c5823fa629ddee7520bf0d5f4fde
Juan Facundo Suarez <facundo@svarez.com.ar>
2014-01-06 21:52:34 -0300

    adding fb app

--- examples/example.php
+++ examples/example.php
@@ -19,8 +19,8 @@ require '../src/facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
-  'appId'  => '344617158898614',
-  'secret' => '6dc8ac871858b34798bc2488200e503d',
+  'appId'  => '191151347752624',
+  'secret' => 'a4c1147272a2df8f706f5ac2a7956b5b',
 ));
 
 // Get User ID
