2dd8178d12670013abc0ff8d3111700cda56f1ef
Juan Facundo Suarez <facundo@svarez.com.ar>
2014-01-07 00:33:45 -0300

    adding fb app like gate

--- index.php
+++ index.php
@@ -40,6 +40,31 @@ if ($user) {
 </head>
 <body>
 
+<script>
+  window.fbAsyncInit = function() {
+    // init the FB JS SDK
+    FB.init({
+      appId      : '191151347752624',                        // App ID from the app dashboard
+      status     : true,                                 // Check Facebook Login status
+      xfbml      : true                                  // Look for social plugins on the page
+    });
+
+    // Additional initialization code such as adding Event Listeners goes here
+
+FB.Canvas.setAutoGrow(1000);
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
   <?php if (!$isFan): ?>
     <div class="fb_like_mask"></div>
   <?php endif ?>
