406fe47a01cd744ca692788debebf7053b3c38aa
Juan Facundo Suarez <facundo@svarez.com.ar>
2014-01-08 19:47:28 -0300

    adding footer content

--- index.php
+++ index.php
@@ -200,7 +200,7 @@ FB.Canvas.setAutoGrow(1000);
     <form action="odeslat.php" method="post" id="formular">
       <fieldset>
         <label for="input-one" class="float"></label><br />
-        <input class="inp-text" placeholder="Full Name"name="one" id="one" type="text" size="30" /><br />
+        <input class="inp-text" placeholder="Full Name" name="one" id="one" type="text" size="30" /><br />
 
         <label for="input-two" class="float"></label><br />
         <input class="inp-text" name="two"  id="two" type="text" size="30" /><br />
