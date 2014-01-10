3c2275e4509c36e91f23651b42dfec242e5bd3df
Juan Facundo Suarez <facundo@svarez.com.ar>
2014-01-10 11:59:27 -0300

    adding adjusting styles

--- index.php
+++ index.php
@@ -156,7 +156,8 @@ FB.Canvas.setAutoGrow(1000);
         <p>02</p>
         <h2>EDUCATE<br />YOURSELF</h2>
         <p>Read as much as possible and talk to others.
-Visit: <a href="http://climate.nasa.gov/">climate.nasa.gov</a> and <a href="http://grist.org/">grist.org</a></p>
+Visit: <a href="http://climate.nasa.gov/" target="_blank">climate.nasa.gov</a>
+ and <a href="http://grist.org/" target="_blank">grist.org</a></p>
       </div>
 
       <div class="pledge_child">
