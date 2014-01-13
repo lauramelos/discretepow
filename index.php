8f655b0c24e098f2721e930a60720cebdfdaa40b
lauramelos <lauramelos@gmail.com>
2014-01-13 20:02:00 -0300

    thanks page

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ $isFan = $request['page']['liked'];
           dataType: "text" 
         }).done(function(data){
           $('.subscribe_mask').show()
-          $("html, body").animate({ scrollTop: 0 }, "slow");
+          window.scrollto( 0 , 0);
         });
         //fadeEffect.init('demoFADE',1);
         //disablePopup();
