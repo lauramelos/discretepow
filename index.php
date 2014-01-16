80986103634f39285218f8615a42051c1ad29b8d
lauramelos <lauramelos@gmail.com>
2014-01-16 18:45:49 -0300

    responsive

--- index.php
+++ index.php
@@ -11,10 +11,9 @@ $request = $facebook->getSignedRequest();
 $isFan = $request['page']['liked'];
 
 ?>
-
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0"/>
   <link rel="stylesheet" type="text/css" href="style.css">
   <script src="inc/jquery-1.10.2.min.js" type="text/javascript"></script>
   <script type="text/javascript" src="https://fast.fonts.net/jsapi/c71e20be-219a-4a86-8783-74514d0a6982.js">
