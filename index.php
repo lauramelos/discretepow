bc581c63fcd55f11a48b8522ee716a3f82f50831
Juan Facundo Suarez <facundo@svarez.com.ar>
2014-01-16 10:55:50 -0300

    styling like button

--- index.php
+++ index.php
@@ -348,5 +348,17 @@ less carbon intensive options.</p>
   </div>
   <div class="sponsors_footer">
   </div>
+
+    <div class="fb-like"
+      data-href="https://www.facebook.com/pages/Yakima-test/399004496820979?id=399004496820979&amp;sk=app_191151347752624"
+      data-width="600"
+      data-layout="standard"
+      data-action="like"
+      data-show-faces="true"
+      data-share="false"
+      data-colorscheme="dark">
+    </div>
+
+
 </body>
 </html>
