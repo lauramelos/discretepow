93eb6218f1f8cd1a3da8eabf8a3cf922db29c511
lauramelos <lauramelos@gmail.com>
2014-01-17 19:29:05 -0300

    js cleanup

--- index.php
+++ index.php
@@ -142,6 +142,7 @@ $isFan = $request['page']['liked'];
 
   function testAPI() {
     FB.api('/me', function(response) {
+      console.log(response);
       FB.api("/me/likes/399004496820979", function(apiResponse){ 
         console.log(apiResponse);
         if (apiResponse.data && apiResponse.data.length > 0){
