cb2a3615ef997184c238827b6f7421f007a2fc68
Juan Facundo Suarez <facundo@svarez.com.ar>
2014-01-17 19:23:02 -0300

    testing instagram app

---
+++ inc/get_images.js
@@ -0,0 +1,66 @@
+(function($){
+  $(document).ready(function(){
+
+    // global vars
+    var start = 0;
+    var count = 12;
+    var filterby;
+
+    // Add pretty photos in the hard-refresh
+    addPrettyPhoto();
+
+    function addPrettyPhoto() {
+      $("a[rel^='prettyPhoto']").prettyPhoto({ social_tools: '' });
+    }
+
+    /**
+     * Load pictures
+     */
+
+    function load(){
+      var data = {
+        start: start,
+        count: count
+      };
+
+      if (filterby) data.filter = filterby;
+
+      $.ajax({
+        type: 'GET',
+        url: 'pictures.php',
+        data: data
+      }).done(renderResponse);
+    }
+
+    function renderResponse(html){
+      $('#container .show-more').before(html);
+      addPrettyPhoto();
+    }
+
+    /**
+    * Bind 'click' event in show-more button
+    */
+
+    var button = $('#container .show-more a');
+    button.on('click', function(ev){
+      ev.preventDefault();
+      start = $('#container .rectangle').length;
+      load();
+    });
+
+    /**
+    * Bind filters
+    */
+    var filter = $('select.filter');
+
+    filter.on('change', function(e){
+      e.preventDefault();
+      var category = $(this).val();
+      $('#container').find('li.rectangle').remove();
+      filterby = category == 'all' ? null : category;
+      start = 0;
+      load();
+    });
+
+  });
+})(jQuery);
