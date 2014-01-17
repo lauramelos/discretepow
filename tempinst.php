9d963892a575a6476119f508dfa7306453df4448
Juan Facundo Suarez <facundo@svarez.com.ar>
2014-01-17 19:09:49 -0300

    merge

--- tempinst.php
+++ tempinst.php
@@ -1,51 +1,12 @@
 <head>
 
   <script src="inc/jquery-1.10.2.min.js" type="text/javascript"></script>
+  <script src="inc/jquery-1.10.2.min.js" type="text/javascript"></script>
 
 </head>
 
 <?php       
-
-  if ($_GET['next_pics']):
-  $extra_query = '&max_tag_id='.$_GET['next_pics'];
-  endif;
-
-  if ($_GET['prev_pics']):
-  $extra_query = '&min_tag_id='.$_GET['prev_pics'];
-  endif;
-
-  function callInstagram($url)
-    {
-    $ch = curl_init();
-    curl_setopt_array($ch, array(
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_SSL_VERIFYPEER => false,
-    CURLOPT_SSL_VERIFYHOST => 2
-    ));
-
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
-    }
-
-  $tag = 'bjj';
-  $client_id = "11115ed2e74d47bbbebb4c69dbacfae2";
-  $url = "https://api.instagram.com/v1/tags/$tag/media/recent?client_id=$client_id$extra_query";
-
-  echo $url. "<br />";
-  echo "SERVER_NAME : " . $_SERVER['SERVER_NAME'] . "<br />"; 
-  $host = $_SERVER['SERVER_NAME'];
-
-  $inst_stream = callInstagram($url);
-  $results = json_decode($inst_stream, true);
-
-  //Now parse through the $results array to display your results... 
-  foreach($results['data'] as $item){
-      $image_link = $item['images']['low_resolution']['url'];
-      echo '<img src="'.$image_link.'" />';
-  }
-
+  include_once('get_images.php');
   echo $results[pagination][next_url];
   $next_pics = $results[pagination][next_max_tag_id];
   $prev_pics = $results[pagination][min_tag_id];
@@ -53,12 +14,7 @@
   echo "Previous from: $prev_pics";
 ?>
   <a href="http://<?php echo $host; ?>/tempinst.php?prev_pics=<?php echo $prev_pics; ?>">prev 20</a>
-  <a class="next20" href="http://<?php echo $host; ?>/tempinst.php?next_pics=<?php echo $next_pics; ?>">next 20</a>
-  <script>
-  $('.next20').click(function(){ MyFunction(
-    top.window.location = 'https://www.facebook.com/pages/Yakima-test/399004496820979?id=399004496820979&sk=app_191151347752624';
-  ); return false; });
-  </script>
+  <a href="http://<?php echo $host; ?>/tempinst.php?next_pics=<?php echo $next_pics; ?>">next 20</a>
 <?php
     echo '<pre>';
     //print_r($results);
