c4f4bdff668ac5249b84e172341f6916af93662a
Juan Facundo Suarez <facundo@svarez.com.ar>
2014-01-17 12:39:06 -0300

    testign instagram app

--- tempinst.php
+++ tempinst.php
@@ -1,35 +1,4 @@
 <?php       
-    // Get class for Instagram
-    // More examples here: https://github.com/cosenary/Instagram-PHP-API
-    error_reporting(E_ERROR | E_WARNING | E_PARSE);
-    require_once 'inc/instagram.class.php';
-
-    // Initialize class with client_id
-    // Register at http://instagram.com/developer/ and replace client_id with your own
-    $instagram = new Instagram('11115ed2e74d47bbbebb4c69dbacfae2');
-
-    // Set keyword for #hashtag
-    $tag = 'bjj';
-
-    // Get latest photos according to #hashtag keyword
-    $media = $instagram->getTagMedia($tag);
-
-    // Set number of photos to show
-    $limit = 200;
-
-    // Set height and width for photos
-    $size = '300';
-
-    // Show results
-    // Using for loop will cause error if there are less photos than the limit
-    foreach(array_slice($media->data, 0, $limit) as $data)
-    {
-        // Show photo
-        echo '<img style="float: left; display: block;" src="'.$data->images->thumbnail->url.'" height="'.$size.'" width="'.$size.'" alt="SOME TEXT HERE" />';
-    }
-
-    echo '<p style="display: block; clear: both;">end class testing</p>';
-    echo '<p style="display: block; clear: both;">start direct testing</p>';
 
  function callInstagram($url)
     {
