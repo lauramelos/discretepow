b0acefed396310418212f39fa432e6c9e5c94f7f
lauramelos <lauramelos@gmail.com>
2014-01-20 19:55:19 -0300

    remove likegate on ios chrome

--- index.php
+++ index.php
@@ -131,12 +131,7 @@ $isFan = $request['page']['liked'];
         top.window.location = 'https://www.facebook.com/discreteheadwear/app_476755032431099'
       });
     }
-    if(navigator.userAgent.match('CriOS')){
-          $('.fb_like_mask').hide();
-          $('.fb-like').hide();
-          $('.fb-login-button').hide();
-    }
-  };
+      };
   // Load the SDK asynchronously
   (function(d){
    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
@@ -144,6 +139,12 @@ $isFan = $request['page']['liked'];
    js = d.createElement('script'); js.id = id; js.async = true;
    js.src = "//connect.facebook.net/en_US/all.js";
    ref.parentNode.insertBefore(js, ref);
+   if(navigator.userAgent.match('CriOS')){
+          $('.fb_like_mask').hide();
+          $('.fb-like').hide();
+          $('.fb-login-button').hide();
+    }
+
   }(document));
 
   function testAPI() {
